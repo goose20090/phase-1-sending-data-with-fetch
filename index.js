5440d2c6734ca93e61e2bbfdb9310c80a498ad59
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-06-15 18:29:01 +0100

    commit tree 90ffe3f40f6ae0a5a64063ba31b4c2c8f355a00b

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+
+const { bodyParser } = require("json-server")
+
+function submitData(userName, userEmail){
+    const configurationObject= {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body:JSON.stringify({
+            "name": userName,
+            "email": userEmail
+        })
+    }
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(body) {
+        return addToDOM(body.id)
+    })
+    .catch(function(message){
+        return addToDOM(message);
+    })
+
+};
+
+function addToDOM(newID){
+    document.getElementsByTagName("body")[0].innerHTML= newID;
+    document.body.append(newID)
+}
